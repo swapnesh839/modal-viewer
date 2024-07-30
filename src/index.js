5131e542a926ab61785d18230d1b1a5de3fecbd7
swapnesh <swapnesha839@gmail.com>
2024-07-30 11:45:21 +0530

    update

--- src/index.js
+++ src/index.js
@@ -3,19 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import ARViewer from './ARViewer';
-import { BrowserRouter, Routes ,Route } from 'react-router-dom';
-import Home from './Home';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/ar" element={<ARViewer />} />
-        <Route path="*" element={<Home />} />
-      </Routes>
-    </BrowserRouter>
+    <ARViewer />
   </React.StrictMode>
 );
 
